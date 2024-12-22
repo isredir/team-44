8ed0de6e456174a8a69989e94f9bd6e40dcb69e7
isredir <115943938+isredir@users.noreply.github.com>
2024-12-22 17:09:31 +0300

    Update spark_setup.sh

--- spark_setup/spark_setup.sh
+++ spark_setup/spark_setup.sh
@@ -7,7 +7,6 @@ echo "Распаковываем Spark..."
 tar -xzf "spark-3.5.3-bin-hadoop3.tgz"
 
 # установка переменных JAVA_HOME и HADOOP_HOME
-echo 'export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64' >> ~/hadoop-3.4.0/etc/hadoop/hadoop-env.sh
 export HADOOP_HOME=/home/hadoop_test/hadoop-3.4.0
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin
@@ -18,7 +17,7 @@ echo 'export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64' >> ~/.profile
 echo 'export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin' >> ~/.profile
 
 echo 'export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop' >> ~/.profile
-echo 'export SPARK_HOME=/home/hadoop_test/spark-3.5.3-bin-hadoop3' >> .profile
-echo 'export PATH=$PATH:$SPARK_HOME/bin' >> .profile
-echo 'export SPARK_DIST_CLASSPATH=$SPARK_HOME/jars/*:$(hadoop classpath)' >> .profile
-echo 'PYSPARK_SUBMIT_ARGS="--master local[2] pyspark-shell"' >> .profile
+echo 'export SPARK_HOME=/home/hadoop_test/spark-3.5.3-bin-hadoop3' >> ~/.profile
+echo 'export PATH=$PATH:$SPARK_HOME/bin' >> ~/.profile
+echo 'export SPARK_DIST_CLASSPATH=$SPARK_HOME/jars/*:$(hadoop classpath)' >> ~/.profile
+echo 'PYSPARK_SUBMIT_ARGS="--master local[2] pyspark-shell"' >> ~/.profile
